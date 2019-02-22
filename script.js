2eacf5f2d8b21b54f4cff65fc9d4b47d69813963
etubom <way2young4u@hotmail.com>
2019-02-22 00:55:43 +0000

    changed method to click

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
 function setGradient() {
+  debugger;
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
   css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+color1.addEventListener("click", setGradient);
+color2.addEventListener("click", setGradient);
